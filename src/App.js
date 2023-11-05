c07b894c0e352d3d433be68ebe2f95a785821842
Lakshan-K <lakshan67@hotmail.com>
2023-11-04 20:47:36 -0400

    make changes to the transaction input and styling

--- src/App.js
+++ src/App.js
@@ -22,32 +22,32 @@ function App() {
         <div className = "transaction">
           <div className = "left">
             <div className = "name">New Samsung TV</div>
-            <div className = "description">It's time for a new TV!</div>
+            <div className = "description">It was time for a new TV.</div>
           </div>
           <div className = "right">
-            <div className ="price">$500</div>
+            <div className ="price red">-$500</div>
             <div className = "datetime">2023-11-04</div>
           </div>
         </div>
 
         <div className = "transaction">
           <div className = "left">
-            <div className = "name">New Samsung TV</div>
-            <div className = "description">It's time for a new TV!</div>
+            <div className = "name">Purchased a Website for my Business</div>
+            <div className = "description">Taking my business to the next level!</div>
           </div>
           <div className = "right">
-            <div className ="price">$500</div>
+            <div className ="price">-$999</div>
             <div className = "datetime">2023-11-04</div>
           </div>
         </div>
 
         <div className = "transaction">
           <div className = "left">
-            <div className = "name">New Samsung TV</div>
-            <div className = "description">It's time for a new TV!</div>
+            <div className = "name">iPhone 15</div>
+            <div className = "description">Needed the hottest phone of 2023!</div>
           </div>
           <div className = "right">
-            <div className ="price">$500</div>
+            <div className ="price red">-$1300</div>
             <div className = "datetime">2023-11-04</div>
           </div>
         </div>
